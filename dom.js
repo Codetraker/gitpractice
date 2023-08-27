dad20321a33afeb4f76a1e20810418d41bc8e80a
Codetraker <swarajbibhuprasad@gmail.com>
2023-08-27 20:19:34 +0530

    bold and background of list item-3 change

--- dom.js
+++ dom.js
@@ -96,3 +96,19 @@ for(var i = 0; i<even.length ; i++){
     even[i].style.backgroundColor = '#f4f4f4';
 }
 */
+//-------------------------------------------------------------
+//task-3
+//let header = document.getElementById('main-header');
+//header.style.border = 'solid 3px #000';
+
+//var title = document.querySelectorAll('.title');
+//title[0].textContent = 'ADD ITEM';
+//title[0].style.fontWeight = 'bold';
+//title[0].style.backgroundColor = 'green';
+//-----------------------------------------------------------
+//task-4
+let thirditem = document.getElementsByClassName('list-group-item');
+thirditem[2].style.backgroundColor = 'green';
+
+let bolditem = document.getElementById('items');
+bolditem.style.fontWeight = 'bold';
