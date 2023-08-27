2a2518bf776e1a72b6e69c14d49fc6aa33439744
Codetraker <swarajbibhuprasad@gmail.com>
2023-08-27 23:53:35 +0530

    query selector task-6

--- dom.js
+++ dom.js
@@ -114,7 +114,7 @@ for(var i = 0; i<even.length ; i++){
 //bolditem.style.fontWeight = 'bold';
 //-------------------------------------------------------------
 //task-5
-let items = document.getElementsByClassName('list-group-item');
+/*let items = document.getElementsByClassName('list-group-item');
 for(let i=0; i<items.length;i++){
     items[i].style.backgroundColor = '#f4f4f4';
 }
@@ -127,4 +127,21 @@ fiveitems[0].style.padding ='10px';
 let tagged = document.getElementsByTagName('li');
 for(let i=0; i<tagged.length;i++){
     tagged[i].style.fontWeight='bold';
+}*/
+//-------------------------------------------------------------------
+//task-6
+let secitem = document.querySelector('.list-group-item:nth-child(2)');
+secitem.style.backgroundColor = 'green';
+
+let thirditem = document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.display = 'none';
+//-----------------------------------------------------------------
+//Don't run all the code. 
+//------------------------------------------------------------------
+let secfont = document.querySelectorAll('.list-group-item');
+secfont[1].style.color = 'green';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i = 0; i<odd.length ; i++){
+    odd[i].style.backgroundColor = 'green';
 }
