66c5974b52a2e650a3b6d6c0cb3fbc3d51dd5d1d
Codetraker <swarajbibhuprasad@gmail.com>
2023-08-28 20:15:43 +0530

    assignment

--- dom.js
+++ dom.js
@@ -219,3 +219,30 @@ for(let i = 0; i<odd.length ; i++){
 //container.insertBefore(newDiv,h1);
 
 //console.log(newDiv);
+
+//PART - 2 Assignment--->
+//Q1
+/*
+let newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('title','HelloDiv');
+let newDivText = document.createTextNode('HEllo World');
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+console.log(container);
+console.log(h1);
+
+//Q2
+let newDiv2 = document.createElement('li');
+newDiv2.className = 'list-group-item';
+let newDivText2 = document.createTextNode('HEllo World');
+newDiv2.appendChild(newDivText2);
+
+let itemsList = document.querySelector(".list-group");
+let item1=itemsList.firstChild;
+itemsList.insertBefore(newDivText2, item1);
+*/
